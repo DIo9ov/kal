df22f38eeebbebef74996e1b978d5d5d49b62ce0
DIo9ov <dimidragon9999@gmail.com>
2023-02-10 11:32:29 +0200

    neshto_pisah_kalkulator

--- bachka_neshto_tuka.js
+++ bachka_neshto_tuka.js
@@ -1,1 +1,42 @@
-console.log("Hello world");
+let inputA = document.getElementById("inputA");
+let inputB = document.getElementById("inputB");
+let sumButton = document.getElementById("sumButton");
+let razButton = document.getElementById("razButton");
+let delButton = document.getElementById("delButton");
+let umnButton = document.getElementById("umnButton");
+let result = document.getElementById("result");
+sumButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a + b;
+	result.innerText = c;
+
+}
+razButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a - b;
+	result.innerText = c;
+
+}
+delButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a / b;
+	result.innerText = c;
+
+}
+umnButton.onclick = function() {
+
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	let c = a * b;
+	result.innerText = c;
+
+}
+
+
+
