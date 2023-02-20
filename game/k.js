e865c155ffee4d469a31127ba767ac22d9578255
DIo9ov <dimidragon9999@gmail.com>
2023-02-20 12:28:19 +0200

    sonic_w_bg_karta

--- game/k.js
+++ game/k.js
@@ -1,24 +1,46 @@
 let canvas = document.getElementById("gCanvas");
 let context = canvas.getContext("2d");
-let nx = 4;
-let ny = 6;
-let sqside = 50;
-
+let nx = 25;
+let ny = 15;
+let sqside = 65;
 let hx=3;
 let hy=2;
-
-
+let himg = new Image();
+himg.src = "h.png";
 canvas.width=nx*sqside;
 canvas.height=ny*sqside;
-	for (let i = 0; i < nx; i++)
-	{
-		for (let j = 0; j < ny; j++)
-		{
-			context.strokeRect(i * sqside, j * sqside, sqside, sqside);
-		}
-	}
+let bgimg = new Image();
+bgimg.src = "bg_karta.jpg";
+function drawMap() {
 
-//context.fillRect(hx * sqside, hy * sqside, sqside, sqside);
-let himg = new Image();
-himg.src = "h.png";	
-context.drawImage(himg,hx * sqside, hy * sqside, sqside, sqside);
+context.clearRect(0, 0, canvas.width, canvas.height);
+context.drawImage(bgimg, 0,0,canvas.width, canvas.height);
+context.drawImage(himg, hx * sqside, hy * sqside, sqside, sqside);
+
+
+
+for (let i = 0; i < nx; i++)
+{
+	for (let j = 0; j < ny; j++)
+	{
+		context.strokeRect(i * sqside, j * sqside, sqside, sqside);
+	}
+}
+
+}
+
+
+drawMap();
+
+
+canvas.onclick = function(d) {
+	let x = d.x - canvas.offsetLeft;
+	let y = d.y - canvas.offsetTop;
+	hx = Math.floor(x / sqside);
+	hy = Math.floor(y / sqside);
+	console.log(hx + " " + hy);
+	drawMap();
+}
+
+
+
