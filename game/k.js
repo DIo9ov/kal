0f2430401777fd71a9499efc42eaf945d3533fed
DIo9ov <dimidragon9999@gmail.com>
2023-02-21 15:38:41 +0200

    Dobawih_gabrovo_na_kartata

--- game/k.js
+++ game/k.js
@@ -1,8 +1,11 @@
 let canvas = document.getElementById("gCanvas");
+let menu = document.getElementById("menu");
 let context = canvas.getContext("2d");
-let nx = 25;
-let ny = 15;
-let sqside = 65;
+let contextMenu = menu.getContext("2d");
+
+let nx = 40;
+let ny = 25;
+let sqside = 22;
 let hx=3;
 let hy=2;
 let himg = new Image();
@@ -11,6 +14,18 @@ canvas.width=nx*sqside;
 canvas.height=ny*sqside;
 let bgimg = new Image();
 bgimg.src = "bg_karta.jpg";
+
+
+
+let gabrovo = new Image();
+gabrovo.src = "gabrovo.jpg";
+
+function drawObekt(){
+
+contextMenu.drawImage(gabrovo, 0,0);
+
+}
+
 function drawMap() {
 
 context.clearRect(0, 0, canvas.width, canvas.height);
@@ -40,6 +55,62 @@ canvas.onclick = function(d) {
 	hy = Math.floor(y / sqside);
 	console.log(hx + " " + hy);
 	drawMap();
+}
+
+
+function moveLeft(){
+	if(hx>0)
+	{	
+	hx = (hx - 1);
+	drawMap();
+	}	
+}
+function moveDown(){
+	if(hy<ny-1)
+	{	
+	hy = (hy + 1);
+	drawMap();
+	}
+}
+function moveRight(){
+	if(hx<nx-1)
+	{
+	hx = (hx + 1);
+	drawMap();
+	}
+}
+function moveUp(){
+	if(hy>0)
+	{
+	hy = (hy - 1);
+	drawMap();
+	}	
+
+}
+
+function mqsto(){
+if (hx==19 && hy==10)
+	{
+			console.log ("В Габрово");
+			drawObekt()
+	}
+}
+
+document.onkeypress = function(e){
+	let key = e.key;
+		switch(key){
+			case "4": moveLeft(); break;
+			case "2": moveDown(); break;
+			case "6": moveRight(); break;
+			case "8": moveUp(); break;
+			case "a": moveLeft(); break;
+			case "s": moveDown(); break;
+			case "d": moveRight(); break;
+			case "w": moveUp(); break;
+			}
+			
+	console.log (hx + " " + hy);
+	mqsto();
 }
 
 
